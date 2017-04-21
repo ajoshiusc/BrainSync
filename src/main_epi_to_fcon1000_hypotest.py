c72c00c363d757ac28b0161129b9b6e0a28983e7
Anand A Joshi <ajoshi@usc.edu>
2017-04-20 19:08:39 -0700

    replaced test stat with avg correlation

--- src/main_epi_to_fcon1000_hypotest.py
+++ src/main_epi_to_fcon1000_hypotest.py
@@ -5,22 +5,22 @@ Created on Thu Aug  4 05:39:43 2016
 @author: ajoshi
 """
 import sys
-sys.path.append('/big_disk/ajoshi/coding_ground/cortical_parcellation/src/')
-
 import scipy.io
 import scipy as sp
 import os
 import numpy as np
 import nibabel as nib
 from dfsio import readdfs, writedfs
-from surfproc import view_patch, view_patch_vtk, smooth_surf_function, face_v_conn, patch_color_attrib
+from surfproc import view_patch, view_patch_vtk, smooth_surf_function,\
+                     face_v_conn, patch_color_attrib
 from fmri_methods_sipi import rot_sub_data, reorder_labels
 import matplotlib.pyplot as plt
 from scipy.ndimage.filters import gaussian_filter
 import glob
+from statsmodels.stats.multitest import multipletests
 
 p_dir = '/big_disk/ajoshi/HCP_data'
-p_dir_ref='/big_disk/ajoshi/HCP_data/'
+p_dir_ref = '/big_disk/ajoshi/HCP_data/'
 lst = os.listdir(p_dir)
 r_factor = 3
 ref_dir = os.path.join(p_dir_ref, 'reference')
@@ -33,13 +33,6 @@ dfs_right = readdfs(os.path.join(p_dir_ref, 'reference', ref + '.aparc\
 .a2009s.32k_fs.reduce3.right.dfs'))
 dfs_right_sm = readdfs(os.path.join(p_dir_ref, 'reference', ref + '.aparc\
 .a2009s.32k_fs.reduce3.very_smooth.right.dfs'))
-
-ind_subsample = sp.arange(start=0, stop=dfs_right.labels.shape[0], step=1)
-ind_rois_orig = sp.in1d(dfs_right.labels,[46,3,4,28,29,68,69,70])
-ind_rois = sp.full(ind_rois_orig.shape[0], False ,dtype=bool)
-ind_rois = ind_rois_orig.copy()
-ind_rois[ind_subsample] = True
-
 
 surf1 = dfs_right_sm
 X = surf1.vertices[:, 0]
@@ -74,14 +67,13 @@ surf1.attributes = surf_weight
 surf_weight = surf_weight[:, None]
 # smooth_surf_function(dfs_right_sm, Wt*Ar*0.1, a1=0, a2=1)
 
-surf1.attributes = ind_rois
-surf1 = patch_color_attrib(surf1)
-view_patch_vtk(surf1, show=1)
-
 # sub = '110411'
 # p_dir = '/home/ajoshi/data/HCP_data'
 lst = os.listdir('/big_disk/ajoshi/HCP5')
-rho1 = 0; rho1rot = 0; rho2 = 0; rho2rot = 0;
+rho1 = 0
+rho1rot = 0
+rho2 = 0
+rho2rot = 0
 # lst = [lst[0]]
 diffbefore = 0
 diffafter = 0
@@ -91,14 +83,13 @@ sub = lst[0]
 vrest1 = scipy.io.loadmat('/big_disk/ajoshi/coding_ground/epilepsy/\
 NorthShoreLIJ/0019002/fmri_tnlm_5_reduce3_v2.mat')  # h5py.File(fname1);
 
-                          
+
 data = vrest1['func_right']
 indx = sp.isnan(data)
 data[indx] = 0
 
 
 vrest = data
-vrest = vrest[ind_rois, ]
 m = np.mean(vrest, 1)
 vrest = vrest - m[:, None]
 s = np.std(vrest, 1)+1e-116
@@ -109,12 +100,12 @@ rho1rot = 0
 diffafter = 0
 diffbefore = 0
 
-a = sp.load('fcon1000_null_right.npz')
-rho_null=a['rho_null']
+a = sp.load('fcon1000_null_all_right.npz')
+rho_null = sp.mean(a['rho_null'], axis=0)
 
 lst = glob.glob('/big_disk/ajoshi/fcon_1000/Beijing/sub*')
 nsub = 0
-rho_all = sp.zeros((vrest1.shape[0],0))
+rho_all = sp.zeros((vrest1.shape[0], 0))
 
 #sub = lst[10]
 #vrest2 = scipy.io.loadmat(sub + '/fmri_tnlm_5_reduce3_v2.mat')  # h5py.File(fname1);
@@ -131,28 +122,28 @@ rho_all = sp.zeros((vrest1.shape[0],0))
 for sub in lst:
     if not os.path.exists(sub + '/fmri_tnlm_5_reduce3_v2.mat'):
         continue
-    
-    vrest2 = scipy.io.loadmat(sub + '/fmri_tnlm_5_reduce3_v2.mat')  # h5py.File(fname1);
+
+    vrest2 = scipy.io.loadmat(sub + '/fmri_tnlm_5_reduce3_v2.mat')
     data = vrest2['func_right']
     indx = sp.isnan(data)
     data[indx] = 0
     vrest = data
-    vrest = vrest[ind_rois, :vrest1.shape[1]]
+    vrest = vrest[:, :vrest1.shape[1]]
     m = np.mean(vrest, 1)
     vrest = vrest - m[:, None]
     s = np.std(vrest, 1)+1e-116
     vrest2 = vrest/s[:, None]
-    
+
     rho1 += sp.sum(vrest1*vrest2, axis=1)/vrest1.shape[1]
     diffbefore += vrest1 - vrest2
-    
+
     vrest2, Rot = rot_sub_data(ref=vrest1, sub=vrest2,
-                                area_weight=sp.sqrt(surf_weight[ind_rois]))    
-    t = sp.sum(vrest1*vrest2, axis=1)/vrest1.shape[1]    
-    
-    rho_all = sp.append(rho_all, t[:,None], axis=1)
+                               area_weight=sp.sqrt(surf_weight))
+    t = sp.sum(vrest1*vrest2, axis=1)/vrest1.shape[1]
+
+    rho_all = sp.append(rho_all, t[:, None], axis=1)
     rho1rot += t
-    
+
     diffafter += vrest1 - vrest2
     nsub += 1
     print sub
@@ -160,20 +151,20 @@ for sub in lst:
 rho1rot /= nsub
 
 
-
-import seaborn as sns
-
-sns.distplot(rho_all[120,:])
-sns.distplot(rho_null[120,:])
+#
+# import seaborn as sns
+#
+# sns.distplot(rho_all[120,:])
+# sns.distplot(rho_null[120,:])
 
 #
-#pval=sp.zeros((rho_all.shape[0],1))
+# pval=sp.zeros((rho_all.shape[0],1))
 #
-#for jj in range(rho_all.shape[0]):
-#    _, pval[jj] = sp.stats.mannwhitneyu(rho_null[jj,:], rho_all[jj,:]) #,
-#                                        # alternative='greater')
-#    print jj
-#sns.distplot(pval)
+# for jj in range(rho_all.shape[0]):
+#     _, pval[jj] = sp.stats.mannwhitneyu(rho_null[jj,:], rho_all[jj,:]) #,
+#                                         # alternative='greater')
+#     print jj
+# sns.distplot(pval)
 #
 dfs_right_sm.attributes = sp.squeeze(rho_all.mean(axis=1))
 dfs_right_sm = patch_color_attrib(dfs_right_sm, clim=[0, 1])
@@ -182,30 +173,28 @@ view_patch_vtk(dfs_right_sm, azimuth=90, elevation=180, roll=90,
 view_patch_vtk(dfs_right_sm, azimuth=-90, elevation=180, roll=-90,
                outfile='rest_fcon1000_0019002_right.png', show=1)
 
-
-rho_all1 = sp.mean(rho_all,axis=1)[:,None]
-pval = 0*rho_all1;
+rho_null = rho_null.T
+rho_all1 = sp.mean(rho_all, axis=1)[:, None]
+pval = 0*rho_all1
 
 for jj in range(rho_all1.shape[1]):
-    pval[:,jj] = sp.sum(rho_null > rho_all1[:, jj][:,None], axis=1) #,
+    pval[:, jj] = sp.sum(rho_null > rho_all1[:, jj][:, None], axis=1)
 
 pval1 = 1-pval.mean(axis=1)/rho_null.shape[1]
-from statsmodels.stats.multitest import multipletests
-a=multipletests(pvals=pval1, alpha=0.05, method='fdr_bh')
+a = multipletests(pvals=pval1, alpha=0.05, method='fdr_bh')
 dfs_right_sm.attributes = pval1
-sns.distplot(pval1)
+# sns.distplot(pval1)
 
-#from rpy2.robjects.packages import importr
-#from rpy2.robjects.vectors import FloatVector
+# from rpy2.robjects.packages import importr
+# from rpy2.robjects.vectors import FloatVector
 #
-#stats = importr('stats')
+# stats = importr('stats')
 #
-#p_adjust = stats.p_adjust(FloatVector(pval1), method = 'BH')
-#dfs_right_sm.attributes= sp.array(p_adjust)
+# p_adjust = stats.p_adjust(FloatVector(pval1), method = 'BH')
+# dfs_right_sm.attributes= sp.array(p_adjust)
 
-dfs_right_sm = patch_color_attrib(dfs_right_sm, clim=[0, .05])
+dfs_right_sm = patch_color_attrib(dfs_right_sm, clim=[0, 0.05])
 view_patch_vtk(dfs_right_sm, azimuth=90, elevation=180, roll=90,
                outfile='rest_after_rot1_fcon1000_0019002_right.png', show=1)
 view_patch_vtk(dfs_right_sm, azimuth=-90, elevation=180, roll=-90,
                outfile='rest_after_rot2_fcon1000_0019002_right.png', show=1)
-
