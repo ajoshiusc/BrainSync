8d0c7448c3150430bdc6ba7852a73b23ea169df4
Anand A Joshi <ajoshi@erdos.usc.edu>
2018-03-10 21:24:44 -0800

    update

--- src/main_dyn_conn.py
+++ src/main_dyn_conn.py
@@ -17,13 +17,20 @@ NCMP = 21
 surfObj = readdfs(join(BFPPATH, 'supp_data', 'bci32kleft.dfs'))
 numVert = len(surfObj.vertices)
 
-sub1n = '/big_disk/ajoshi/HCP100/HCP100/135932/MNINonLinear/Results/rfMRI_REST1\
-_LR/rfMRI_REST1_LR_Atlas_hp2000_clean.dtseries.nii'
+sub1n = '/deneb_disk/HCP/100307/MNINonLinear/Results/rfMRI_REST1_LR/rfMRI_REST1_LR_Atlas_hp2000_clean.dtseries.nii'
+sub1n_tsk = '/deneb_disk/HCP/100307/MNINonLinear/Results/tfMRI_MOTOR_LR/tfMRI_MOTOR_LR_Atlas.dtseries.nii'
+
 #sub1 = nilearn.image.load_img(sub1n)
 sub1 = nib.cifti2.cifti2.load(sub1n)
 X = sub1.get_data().T
 Xorig = np.array(X)
 X, _, _ = normalizeData(X.T)
+
+
+sub1tsk = nib.cifti2.cifti2.load(sub1n_tsk)
+Xtsk = sub1tsk.get_data().T
+Xorig = np.array(X)
+Xtsk, _, _ = normalizeData(Xtsk.T)
 
 # %% Explained variance
 _, s, _ = np.linalg.svd(np.dot(X, X.T))
@@ -54,12 +61,12 @@ plt.title('orig')
 # D is the representative data that produces the network
 #DR = p.fit_transform(XR.T)
 '''
-nT = X.shape[0]
+nT = Xtsk.shape[0]
 
-Xnew = np.zeros(X.shape)
+Xnew = np.zeros(Xtsk.shape)
 Cind = np.int((NCMP-1)/2+1)
-for i in range(X.shape[0]-NCMP):
-    xin = X[i:i+NCMP, :]
+for i in range(Xtsk.shape[0]-NCMP):
+    xin = Xtsk[i:i+NCMP, :]
     xin, _, nrm = normalizeData(xin)
     dd, _ = brainSync(xin, D)
     dd = dd*nrm
@@ -67,7 +74,7 @@ for i in range(X.shape[0]-NCMP):
     print("%d" % i, end=',')
 
 a = nib.cifti2.Cifti2Image(Xnew, sub1.header, file_map=sub1.file_map)
-a.to_filename('outfile.nii')
+a.to_filename('outfile_task.nii')
 
 '''
 % 
